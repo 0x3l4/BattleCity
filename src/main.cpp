bfe3145480eeb6e4cf09bb976a28aef04566b93d
= <alexander.git@yandex.ru>
2023-10-20 13:06:56 +0500

    Changed main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,14 +4,12 @@
 
 int main(void)
 {
-    GLFWwindow* window;
-
     /* Initialize the library */
     if (!glfwInit())
         return -1;
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
